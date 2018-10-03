3d0b01f86b95def0d5df03d3eae3709946954cdd
Andrya_A354 <dddd@gmail.com>
2018-10-03 11:14:27 +0700

    commit 1

---
+++ src/main/java/com/mitrais/lab1/model/OrderCake.java
@@ -0,0 +1,12 @@
+package com.mitrais.lab1.model;
+
+public class OrderCake extends Cake {
+
+    public OrderCake(String name, double rate) {
+        super(name, rate);
+    }
+
+    public double calcPrice() {
+        return 0;
+    }
+}
